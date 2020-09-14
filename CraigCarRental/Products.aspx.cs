ad5a2738cc6c05505b268482f7d4692cf32a2bb7
GYFTDCHYLD <59238976+GYFTDCHYLD@users.noreply.github.com>
2020-09-13 20:46:06 -0500

    Update Products.aspx.cs
    include a few new library

--- CraigCarRental/Products.aspx.cs
+++ CraigCarRental/Products.aspx.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
-namespace CraigCarRental
-{
+namespace CraigCarRental{ 
 
-    public partial class Products : System.Web.UI.Page
-    {
-        
+    public partial class Products : System.Web.UI.Page{
+
     }
 }
