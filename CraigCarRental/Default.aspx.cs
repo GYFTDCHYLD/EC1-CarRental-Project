419e9de0cf57488ae7fb213a4050599a8a1af851
GYFTDCHYLD <59238976+GYFTDCHYLD@users.noreply.github.com>
2020-10-10 11:57:20 -0500

    Revert "Revert "delete query""
    This reverts commit 7e16d96c16a03a2d0e5fc8310369058eee3f6e71.

--- CraigCarRental/Default.aspx.cs
+++ CraigCarRental/Default.aspx.cs
@@ -10,18 +10,15 @@ using System.Collections.Generic;
 using System.Text.Encodings.Web;
 using System.Management.Instrumentation;
 using MySql.Data.MySqlClient;
-
-
+using System.ComponentModel;
 
 namespace CraigCarRental {
 
     public partial class Default : System.Web.UI.Page {
         DataTable dt;
         DataRow dr;
-        Cart cart = new Cart();
         static int days = 0;
-        static Boolean Conected = false;
-        readonly CarDatabase db = new CarDatabase();// initialize a database of cars
+        private string ConnectionString;
 
         public void Page_Load(object sender, EventArgs args) {
             TextBox1.Text = DateTime.Today.ToShortDateString() + '.';
@@ -53,8 +50,10 @@ namespace CraigCarRental {
                 Button button = (Button)sender;
                 string buttonId = button.ID;// get the "ID" from the pressed button
 
-                Rental rentalData = new Rental(new Customer(1, "Craig", "Reid"), db.selectCar(buttonId), days);
-                RentalDatabase(rentalData);
+                ConnectionString = @"Server=localhost;Database=AppDatabase;Uid=root;Pwd=EnterpriseComputing1;";
+
+                Rental rentalData = new Rental(new User(1, "Craig", "Reid", "Admin"), SelectCarQuery(buttonId), days);
+                RentalQuery(rentalData);
 
                 dt = new DataTable();
                 dt = (DataTable)Session["CART"];
@@ -80,10 +79,33 @@ namespace CraigCarRental {
             TextBox1.Text = Calendar1.SelectedDate.ToShortDateString() + '.';
         }
 
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public Car SelectCarQuery(String ID) {
 
-        public void RentalDatabase(Rental rentalData) { 
+            Car car = new Car();
+            try {
+                MySqlConnection connection = new MySqlConnection(ConnectionString);
+                MySqlCommand command = new MySqlCommand("SelectCar", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add("_CarID", MySqlDbType.String).Value = ID;
+                connection.Open();
+                MySqlDataReader dr = command.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dr.Read()){
+                    car = new Car(Convert.ToString(dr["CarID"]), Convert.ToString(dr["CarName"]), (float)(dr["Price"]), Convert.ToString(dr["Category"]), Convert.ToString(dr["Discription"]));
+                }
+                dr.Close();
+                return car;
+
+            } catch (Exception e){
+
+            }
+
+            return car;
+        }
+
+
+        public void RentalQuery(Rental rentalData) {  
             //string ConnectionString = @"server=127.0.0.1;" + @"uid=root;" + @"pwd=EnterpriseComputing1;" + @"database=AppDatabase;";
-            string ConnectionString = @"Server=localhost;Database=AppDatabase;Uid=root;Pwd=EnterpriseComputing1;";
 
             try {
                 using (MySqlConnection sqlConnection = new MySqlConnection(ConnectionString)){
